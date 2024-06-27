a23ff622d5930a2639d14900bfc067fc1ceeb5f0
Were Samson Bruno <werefast2000@gmail.com>
2024-06-27 21:49:55 +0000

    change URL of whatsapp API

--- my-app/app/api/whatsapp/route.ts
+++ my-app/app/api/whatsapp/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   try {
-    const response = await fetch('https://intelli-python-backend-zwyu.onrender.com/dashboard/conversations/whatsapp/');
+    const response = await fetch('https://intelli-python-backend-56zq.onrender.com/dashboard/conversations/whatsapp/');
     if (!response.ok) {
       throw new Error('Failed to fetch conversations');
     }
