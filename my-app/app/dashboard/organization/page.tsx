1a6a629d35c864c9b0c52c56d534f7c034c4984c
Were Samson Bruno <werefast2000@gmail.com>
2024-12-22 11:19:51 +0300

    style: increase padding in organizations page for improved layout

--- my-app/app/dashboard/organization/page.tsx
+++ my-app/app/dashboard/organization/page.tsx
@@ -4,7 +4,7 @@ import { JoinedOrganizations } from "@/components/MyOrganizations";
 
 export default function Page() {
   return (
-    <div className="container mx-auto px-4 py-4">
+    <div className="container mx-auto px-4 py-8">
       <h1 className="text-xl sm:text-2xl lg:text-3xl font-bold mb-4 sm:mb-6">Organizations</h1>
       <div className="flex w-full flex-col">
         <JoinedOrganizations />
