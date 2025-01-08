6db3a7af063fe77d54f5a96be4171d94eaf98e13
Were Samson Bruno <werefast2000@gmail.com>
2025-01-08 14:36:25 +0300

    style: format code in Page component for improved readability

--- my-app/app/dashboard/page.tsx
+++ my-app/app/dashboard/page.tsx
@@ -1,18 +1,17 @@
-"use client"
-import { DashComponent } from "@/components/component/dash";
-import { Skeleton } from '@/components/ui/skeleton';
-import { Suspense } from 'react';
+'use client'
 
-
+import { DashComponent } from "@/components/component/dash"
+import { Skeleton } from '@/components/ui/skeleton'
+import { Suspense } from 'react'
 
 export default function Page() {
-
   return (
-    <div className=" container mx-auto px-4 py-8">
+    <div className="container mx-auto px-4 py-8">
       <h1 className="text-xl sm:text-2xl lg:text-3xl font-bold mb-4 sm:mb-6">Home</h1>
       <Suspense fallback={<Skeleton className="w-full h-[600px]" />}>
-      <DashComponent />
+        <DashComponent />
       </Suspense>      
     </div>
-  );
+  )
 }
+
