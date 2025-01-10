05bd77b0ccfcef4e6ee8bf219161d06b417c1762
Were Samson Bruno <werefast2000@gmail.com>
2025-01-10 19:29:46 +0300

    feat: add DeleteAssistantDialog component for confirming assistant deletion

---
+++ my-app/components/delete-dialog-assistant.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import React from 'react';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from '@/components/ui/alert-dialog';
+
+interface DeleteAssistantDialogProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+  assistantName: string;
+}
+
+export function DeleteAssistantDialog({
+  isOpen,
+  onClose,
+  onConfirm,
+  assistantName,
+}: DeleteAssistantDialogProps) {
+  return (
+    <AlertDialog open={isOpen} onOpenChange={onClose}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete the assistant
+            &quot;{assistantName}&quot; and remove its data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={onConfirm}>Delete</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
+
