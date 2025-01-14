2e666a663f42e21ece9d3c297c925d30c81d0d6c
Were Samson Bruno <werefast2000@gmail.com>
2025-01-14 17:44:34 +0300

    Replace NextSteps with Onborda provider

--- my-app/components/component/dash.tsx
+++ my-app/components/component/dash.tsx
@@ -3,23 +3,31 @@
 import React from "react";
 import { useState } from "react";
 import Dashboard from "../dashboard/main";
+import { OnbordaProvider, Onborda, useOnborda } from "onborda";
 
 //
 import { NextStepProvider, NextStep, useNextStep } from "nextstepjs";
 import { steps } from "../../utils/tourSteps";
-import ShadcnCustomCard from "../CustomCard";
+import CustomCard from "../CustomCard";
 
 
 export function DashComponent() {
 
   return (
-    <NextStepProvider>
-      <NextStep steps={steps} cardComponent={ShadcnCustomCard}>
-        <div className="space-y-8" id="step1">
+    <OnbordaProvider>
+      <Onborda 
+      steps={steps}
+      showOnborda={true}
+      shadowRgb="55,48,160"
+      shadowOpacity="0.2"
+      cardComponent={CustomCard}
+      cardTransition={{ duration: 2, type: "tween" }}
+      >
+        <div className="space-y-8" id="onborda-step1">
           {/* Dashboard content */}
           <Dashboard />
         </div>
-      </NextStep>
-    </NextStepProvider>
+      </Onborda>
+    </OnbordaProvider>
   );
 }
