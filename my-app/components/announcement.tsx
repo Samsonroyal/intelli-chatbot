a99456958188b9f618a078781dd5e56096f5165a
Were Samson Bruno <werefast2000@gmail.com>
2025-01-20 12:26:15 +0300

    created announcement badge

---
+++ my-app/components/announcement.tsx
@@ -0,0 +1,16 @@
+import { Badge } from "@/components/ui/badge";
+import { Sparkles } from "lucide-react";
+import Link from "next/link";
+
+export const AnnouncementBanner = () => {
+    return (
+        <><div className="flex justify-center"></div><Link href="/dashboard/conversations/whatsapp">
+            <div className="border indigo-600 shadow-sm bg-indigo-200 backdrop-blur-sm rounded-xl px-4 py-1.5 flex items-center space-x-2 cursor-pointer hover:bg-indigo-300 transition-colors">
+                <Badge variant="default" className="bg-white text-black text-xs flex items-center gap-1">
+                New<Sparkles className="h-3 w-3" /> 
+                </Badge>
+                <span className="text-white text-xs">Export all your contacts in pdf</span>
+            </div>
+        </Link></>
+    );
+};
