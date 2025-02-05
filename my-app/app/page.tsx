fba0fe4309e141751c33ac0ac41a4b13e0671cb4
Were Samson Bruno <werefast2000@gmail.com>
2025-02-05 15:09:20 +0300

    refactor: remove unused Head component and associated metadata for cleaner code

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -9,22 +9,6 @@ import Head from "next/head";
 export default function Homepage() {
   return (
     <>
-    <Head>
-        <title>Intelli</title>
-        <meta name="description" content="Effortless intelligent customer support for your business." />
-        <meta property="og:url" content="https://intelliconcierge.com" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Intelli" />
-        <meta property="og:description" content="Effortless intelligent customer support for your business." />
-        <meta property="og:image" content="https://opengraph.b-cdn.net/production/images/2977506a-36c6-48fc-ba42-cecaae987eb6.png?token=yhX75NIxl7JNr-KYOF9OzCI8xIEvLMHhc6wi33o10dw&height=630&width=1200&expires=33274744940" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="intelliconcierge.com" />
-        <meta property="twitter:url" content="https://intelliconcierge.com" />
-        <meta name="twitter:title" content="Intelli" />
-        <meta name="twitter:description" content="Effortless intelligent customer support for your business." />
-        <meta name="twitter:image" content="https://opengraph.b-cdn.net/production/images/2977506a-36c6-48fc-ba42-cecaae987eb6.png?token=yhX75NIxl7JNr-KYOF9OzCI8xIEvLMHhc6wi33o10dw&height=630&width=1200&expires=33274744940" />
-      </Head>
-
       <main className="min-h-screen p-2 bg-white">
       <Home />
       <PopupModal />
