01013ced720f8efe1d49ad9ad29e2b0797c70c27
Were Samson Bruno <werefast2000@gmail.com>
2025-02-10 13:53:54 +0300

    feat: update PreviewLanding component to use YouTube video and adjust border styling

--- my-app/components/sections/preview-landing.tsx
+++ my-app/components/sections/preview-landing.tsx
@@ -9,11 +9,11 @@ export function PreviewLanding() {
       {/* Inner border */}
       <div className="">
      
-            <div className="relative aspect-video rounded-xl overflow-hidden border-8 border-white-50 shadow-sm">
+            <div className="relative aspect-video rounded-xl overflow-hidden border-2 border-blue-500 shadow-xl">
            <AspectRatio ratio={16 / 9} className="overflow-hidden rounded-sm">
             <iframe
               className="w-full h-full"
-              src={`https://drive.google.com/file/d/1uD5qMxCeBDqwLJqDZCxE2JW7qU8Sq1VL/preview?autoplay=&loop=1&controls=0&mute=`}
+              src={`https://www.youtube.com/embed/AJFvlkWfF7Y?si=X2eN-Vey6o9Ocf7Z/preview?autoplay=&loop=1&controls=0&mute=`}
               title="Video Player"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               referrerPolicy="strict-origin-when-cross-origin"
