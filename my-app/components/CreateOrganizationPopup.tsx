20539eedb5d76da3b98c643ae1e1e2fd17ea673b
Were Samson Bruno <werefast2000@gmail.com>
2025-03-06 14:09:57 +0300

    refactor: simplify CreateOrganizationPopup component and improve loading state handling

--- my-app/components/CreateOrganizationPopup.tsx
+++ my-app/components/CreateOrganizationPopup.tsx
@@ -1,63 +1,33 @@
-'use client';
+"use client";
+import { ClerkLoading, CreateOrganization } from "@clerk/nextjs";
+import { useOrganizationList, useOrganization } from "@clerk/nextjs";
 
-import { useState, useEffect } from 'react';
-import { useOrganizationList } from '@clerk/nextjs';
-import {
-  Dialog,
-  DialogTrigger,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogClose,
-} from '@/components/ui/dialog';
-import { CreateOrganization } from '@clerk/nextjs';
-
-const CreateOrganizationPopup = () => {
-  const { userMemberships, isLoaded } = useOrganizationList({
-    userMemberships: { infinite: true },
+export default function CreateOrganizationStep() {
+  const {
+    isLoaded,
+    setActive,
+    userMemberships,
+    userInvitations,
+    userSuggestions,
+  } = useOrganizationList({
+    userMemberships: {
+      infinite: true,
+    },
   });
-  const [showPopup, setShowPopup] = useState(false);
 
-  useEffect(() => {
-    if (isLoaded) {
-      const hasOrganizations = userMemberships?.data?.length > 0;
-      
-      if (!hasOrganizations) {
-        // Only show popup if user has no organizations
-        setTimeout(() => {
-          setShowPopup(true);
-        }, 7000); // 7 second delay
-      }
-    }
-  }, [isLoaded, userMemberships]);
-
-  // Don't render anything if user has organizations
-  if (userMemberships && userMemberships.data && userMemberships.data.length > 0) {
-    return null;
+  if (!isLoaded) {
+    return <p>Loading...</p>;
   }
 
   return (
-    <Dialog open={showPopup} onOpenChange={setShowPopup}>
-      <DialogTrigger asChild>
-        <button className="hidden">Open Dialog</button>
-      </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Let&apos;s get you started.</DialogTitle>
-        </DialogHeader>
-        <CreateOrganization
-          afterCreateOrganizationUrl="/dashboard"
-          skipInvitationScreen={true}
-          hideSlug={true}
-        />
-        <DialogClose asChild>
-          <button className="mt-4 text-red-500 hover:text-red-700 default-transition">
-            Close
-          </button>
-        </DialogClose>
-      </DialogContent>
-    </Dialog>
-  );
-};
+    <div className="flex justify-center">
+      <ClerkLoading>Loading ...</ClerkLoading>
 
-export default CreateOrganizationPopup;
+      <CreateOrganization
+        afterCreateOrganizationUrl="/dashboard/organization"
+        skipInvitationScreen={false}
+        hideSlug={true}
+      />
+    </div>
+  );
+}
