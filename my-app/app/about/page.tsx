f1cdffea0981a80117966ffe29a3087c7f81b5e6
Were Samson Bruno <werefast2000@gmail.com>
2025-04-07 12:50:14 +0300

    feat: create about page with IntelliAIWebsite component and Navbar

---
+++ my-app/app/about/page.tsx
@@ -0,0 +1,13 @@
+
+import IntelliAIWebsite from '@/components/about-component';
+import {Navbar} from '@/components/navbar';
+
+export default async function Page() {
+
+  return (
+    <>
+    <Navbar />  
+      <IntelliAIWebsite />
+    </>
+  );
+}
