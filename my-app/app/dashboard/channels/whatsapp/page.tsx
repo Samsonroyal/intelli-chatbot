b507ac1a2161015169f79bc45e916670f1355db0
Were Samson Bruno <werefast2000@gmail.com>
2025-05-18 14:18:48 +0300

    Rename whatsapp channels page to onboarding

--- my-app/app/dashboard/channels/whatsapp/page.tsx
+++ my-app/app/dashboard/channels/whatsapp/page.tsx
@@ -1,15 +1,15 @@
 "use client";
 import WhatsAppSignup from "@/components/component/whatsappSignup";
-import WhatsappAssistant from "@/components/WhatsappOnboarding";
+import WhatsappOnboarding from "@/components/WhatsappOnboarding";
 import React, { useEffect, useState } from "react";
 
 export default function WhatsappPage() {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <h2 className="text-2xl font-bold mb-6">Create Whatsapp Assistant</h2>
+      <h2 className="text-2xl font-bold mb-6">Get Started With Whatsapp</h2>
       <div className="space-y-4">
-       <WhatsappAssistant />    
+       <WhatsappOnboarding />    
       </div>
     </div>
   );
